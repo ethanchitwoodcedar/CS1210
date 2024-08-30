114baa2f6d4723a7897321359e9652756eac6f79
ethanchitwoodcedar <ethanchitwood@cedarville.edu>
2024-08-30 14:38:52 -0400

    Added Lab1

---
+++ lab1/lab1.cpp
@@ -0,0 +1,14 @@
+/*******************************************************
+ * Summary: Hello Cedarville! Program For CS-1210
+ *
+ * Author: Ethan Chitwood
+ * Created: Aug 2024
+ ******************************************************/
+#include <iostream>
+
+using namespace std;
+
+int main() {
+   cout << "Hello Cedarville!" << endl;
+   return 0;
+}
